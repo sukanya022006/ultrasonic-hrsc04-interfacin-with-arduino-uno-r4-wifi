18bc126c841216c110f146514f3d5cf60bf9e7c7
Sukanya262006 <sukanyabudhiraja26@gmail.com>
2026-02-03 09:12:35 +0530

    Add Pin Configuration in setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
 #include <Arduino.h>
+#define trigPin 9
+#define echoPin 10
 void setup() {
 // write your initialization code here
+    pinMode(trigPin, OUTPUT);
+    pinMode(echoPin, INPUT);
 }
 
 void loop() {
